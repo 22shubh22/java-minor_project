573a6dfcaca2e4d71083724e29b69f7d2724704d
22shubh22 <22shubh22@gmail.com>
2020-11-24 06:40:55 +0530

    Initial

---
+++ src/main/java/com/example/demo/App.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import com.codahale.shamir.Scheme;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.util.Map;
+
+// class Example {
+//   void doIt() {
+//     final Scheme scheme = new Scheme(new SecureRandom(), 5, 3);
+//     final byte[] secret = "hello there".getBytes(StandardCharsets.UTF_8);
+//     final Map<Integer, byte[]> parts = scheme.split(secret);
+//     final byte[] recovered = scheme.join(parts);
+//     System.out.println(new String(recovered, StandardCharsets.UTF_8));
+//   } 
+// }
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void doIt() {
+        final Scheme scheme = new Scheme(new SecureRandom(), 5, 3);
+        final byte[] secret = "hello there".getBytes(StandardCharsets.UTF_8);
+        final Map<Integer, byte[]> parts = scheme.split(secret);
+        final byte[] recovered = scheme.join(parts);
+        System.out.println(new String(recovered, StandardCharsets.UTF_8));
+      }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        doIt();
+    }
+}
